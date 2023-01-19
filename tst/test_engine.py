f277b8fe2d4021ba2578e345a370c6b9b23cb7d0
nandinimundra00 <38012230+nandinimundra00@users.noreply.github.com>
2023-01-19 14:55:21 -0500

    Update test_engine.py

--- tst/test_engine.py
+++ tst/test_engine.py
@@ -1,1 +1,31 @@
+from src.Misc import *
+from src.Num import *
+from src.Sym import *
+from src.consts import *
+global the
+def run_tests():
 
+  def main(options, help, funs):
+      saved = {}
+      fails = 0
+
+      
+      for k,v in cli(settings(help)).items():
+        options[k] = v
+        saved[k] = v
+      print("main options", options)
+      if options["help"]:
+        print(help)
+      else:
+        for what, fun in funs.items():
+          if options["go"] == "all" or what == options["go"]:
+            for k, v in saved.items():
+              options[k] = v
+            Seed = options["seed"]
+            if funs[what]()== False:
+              fails=fails+1
+              print("❌ fail:",what)
+            else:
+              print("✅ pass:",what)
+      
+      exit(fails)
