10ed46a5eafac227d980bc799254d642020bee3b
niteshmishra <nmishra4@ncsu.edu>
2023-01-19 14:59:00 -0500

    Add main, const, list files

---
+++ src/list_util.py
@@ -0,0 +1,25 @@
+#List functions 
+def map(t, fun, u = {}):
+  for k, v in t.items():
+    print(fun(v))
+    v, k = fun(v)
+    temp = k if k else (1 + len(u))
+    u[temp] = v
+  return u
+
+def kap(t, fun, u = {}):
+  for k, v in t.items():
+    v, k = fun(k, v)
+    temp = k if k else (1 + len(u))
+    u[temp] = v
+  return u
+
+def sort(t, fun):
+  t = sorted(t, key = fun)
+  return t
+
+def keys(t):
+  def temp(k):
+    return k
+  return sort(kap(t, temp))
+
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import sys
+from Misc import *
+sys.path.append("../ASE HW 1/")
+from tst.test_engine import *
+
+# the = cli(settings(sys.argv[1:]))
+# print(the)
+run_tests()
+# if the["eg"] != "nothing":
+#     main(the, help, egs)
