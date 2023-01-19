83e830169159ba5cce798d49392e346f356b8633
Shubham Loya <55990000+crmgogo@users.noreply.github.com>
2023-01-19 14:57:31 -0500

    adding eg def

--- tst/test_engine.py
+++ tst/test_engine.py
@@ -29,3 +29,29 @@ def run_tests():
               print("✅ pass:",what)
       
       exit(fails)
+
+  #egs
+
+  egs = {}
+  the = {}
+  def eg(key, str, fun):
+    egs[key] = fun
+    help = getConstant("help")
+    help = help + " -g {}\t{}\n".format(key, str)
+
+  eg("the", "show settings", lambda:oo(the))
+
+  def rand_generate_reset_regenerate_same():
+      num1, num2 = NUM(), NUM()
+      Seed=the['seed']
+      for i in range(1, 1000):
+        num1.add(rand(0, 1))
+      Seed=the['seed']
+      for i in range(1, 1000):
+        num2.add(rand(0, 1))
+      m1 = rnd(num1.mid(), 10)
+      m2 = rnd(num2.mid(), 10)
+      return m1==m2 and .5 == rnd(m1,1) 
+
+  eg("rand","generate, reset, regenerate same", rand_generate_reset_regenerate_same)
+      
