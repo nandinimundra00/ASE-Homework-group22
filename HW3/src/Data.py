bc001d65fa9a70ad0245634cbccb4a3c9ce26b1b
Shubham Loya <55990000+crmgogo@users.noreply.github.com>
2023-02-02 22:43:02 -0500

    Update Data.py

--- HW3/src/Data.py
+++ HW3/src/Data.py
@@ -44,3 +44,24 @@ class DATA:
             return col.rnd(callable(), n_places), col.txt
 
         return kap(cols, fun)
+    
+    def better(self, row1, row2):
+        s1, s2, ys = 0, 0, self.cols.y
+        for col in ys:
+            x = col.norm(row1[col.at])
+            y = col.norm(row2[col.at])
+            s1 = s1 - math.exp(col.w * (x - y) / len(ys))
+            s2 = s2 - math.exp(col.w * (y - x) / len(ys))
+        return s1/len(ys) < s2/len(ys)
+
+    def dist(self, row1, row2, cols=None):
+        n, d = 0, 0
+        for _, col in enumerate(self.cols.x or cols):
+            n = n + 1
+            d = d + col.dist(row1[col.at], row2[col.at]) ** 2
+        return (d / n) ** (1 / 2)
+
+    def clone(self, init=[]):
+        data = DATA(self.cols.names)
+        _ = list(map(data.add, init))
+        return data
