b4749c22704d8261bc3c56b7d3b5a6ca3410a345
nandinimundra00 <38012230+nandinimundra00@users.noreply.github.com>
2023-02-02 22:38:14 -0500

    Adding new test cases to the test_engine.py file

--- HW3/tst/test_engine.py
+++ HW3/tst/test_engine.py
@@ -85,16 +85,57 @@ def run_tests():
   
   def check_data():
     data = DATA('data/input.csv')
-    return len(data.rows) == 398 and len(data.cols.x) == 4 and data.cols.x[0].at == '1' and data.cols.y[0].w == -1 
+    return         len(data.rows) == 398 and data.cols.y[0].w == -1 and data.cols.x[1].at == 1 and len(data.cols.x) == 4
     
   eg("data", "read DATA csv", check_data)
 
-  def checkStats():
-    data = DATA('data/input.csv')
-    for k,cols in [('y',data.cols.y),('x',data.cols.x)]:
-      print(k,"mid",o(data.stats("mid",cols,2 )))
-      print("", "div",o(data.stats("div",cols,2)))
 
-  eg("stats", "stats from DATA", checkStats)
+  def checkClone():
+    data1 = DATA('data/input.csv')
+    data2 = data1.clone(data1.rows)
+    return (len(data1.rows) == len(data2.rows)) and data1.cols.y[1].w == data2.cols.y[1].w and data1.cols.x[1].at == data2.cols.x[1].at and (len(data1.cols.x) == len(data2.cols.x))
+  
+  eg("clone", "duplicate structure", checkClone)
+
+  def checkAround():
+    data = DATA('data/input.csv')
+    print(0,0,o(data.rows[1]))
+    for n, t in enumerate(data.around(data.rows[1])):
+      if n%50 == 0:
+        print(n, rnd(t['dist'],2) ,o(t['row']))
+
+  eg("around", "sorting nearest neighbors", checkAround)
+
+  def checkHalf():
+    data = DATA('data/input.csv')
+    left,right,A,B,mid,c = data.half(data.rows) 
+    print(len(left),len(right),len(data.rows))
+    print(A)
+    print(o(A), c)
+    print(o(mid))
+    print(o(B))
+
+  eg("half", "1-level bi-clustering", checkHalf)
+
+  def checkCluster():
+    data = DATA('data/input.csv')
+    show(data.cluster(),"mid",data.cols.y,1)
+
+  eg("cluster", "N-level bi-clustering", checkCluster)
+
+  def checkOptimize():
+    data = DATA('data/input.csv')
+    show(data.sway(),"mid",data.cols.y,1)
+
+  eg("optimize", "semi-supervised optimization", checkOptimize)
+  main(the, getConstant("help"), egs)
+
+#   def checkStats():
+#     data = DATA('data/input.csv')
+#     for k,cols in [('y',data.cols.y),('x',data.cols.x)]:
+#       print(k,"mid",o(data.stats("mid",cols,2 )))
+#       print("", "div",o(data.stats("div",cols,2)))
+
+#   eg("stats", "stats from DATA", checkStats)
 
   main(the, getConstant("help"), egs)
