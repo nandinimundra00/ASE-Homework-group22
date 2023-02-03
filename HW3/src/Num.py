70cd5e2b6e172439b145708cf97781e5fdda9514
Nitesh Mishra <54522260+nitesh31mishra@users.noreply.github.com>
2023-02-02 22:48:31 -0500

    Update Num

--- HW3/src/Num.py
+++ HW3/src/Num.py
@@ -1,19 +1,24 @@
+import math
+
 class NUM():
-  def __init__(self, at=0, txt=""):
+  def __init__(self, at=None, txt=None):
     s = "NUM"
     self.n = 0
     self.mu = 0
     self.m2 = 0
     self.lo = float('inf')
     self.hi = float('-inf')
-    self.at = at
-    self.txt = txt
-    if self.txt == "":
-      self.w=1
+    if at:
+        self.at = at
     else:
-      if self.txt[-1] == '-':
+        self.at = 0
+    if txt:
+        self.txt = txt
+    else:
+        self.txt = ""
+    if "-" in self.txt:
         self.w = -1
-      else:
+    else:
         self.w = 1
 
   def add(self, n):
@@ -34,8 +39,34 @@ class NUM():
     else:
       return (self.m2/(self.n - 1))**0.5
 
-  def rnd(x, n):
+  def rnd(self, x, n):
     if x == "?":
       return x
     else:
       return round(x, n)
+  
+  def norm(self, n):
+    if(n == "?"):
+      return n
+    else:
+      return (n - self.lo)/(self.hi - self.lo + 1e-32)
+  
+
+  def dist(self, n1, n2):
+    if(n1 == "?" and n2 == "?"):
+      return 1
+    n1 = self.norm(n1)
+    n2 = self.norm(n2)
+    if(n1 == "?"):
+      if(n2 < 0.5):
+        n1 = 1
+      else:
+        n1 = 0
+    if(n2 == "?"):
+      if(n1 < 0.5):
+        n2 = 1
+      else:
+        n2 = 0
+    return abs(n1 - n2)
+  
+    
